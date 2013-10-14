52a51f9c2cebe55178809871d837dc4058550cc3
ngminhy@gmail.com <ngminhy@gmail.com@3bffda71-2a80-eb86-76df-c0d12f75e2bc>
2013-10-14 15:37:01 +0000

    Add Booking

---
+++ TroyLeeMVCEF/Models/BookingOptionModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TroyLeeMVCEF.Models
+{
+    public class BookingOptionModel
+    {
+        public string doctorid { get; set; }
+        public int year { get; set; }
+        public int month { get; set; }
+        public int day { get; set; }
+
+    }
+}
